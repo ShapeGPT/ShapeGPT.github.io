db1645ea76ac8ea2191acf6e5f6427d0f1551fb2
yaic <91652696+fukunyin@users.noreply.github.com>
2023-11-30 16:03:22 +0800

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -188,7 +188,7 @@ export default function IndexPage() {
 
 
       <div className="w-full items-end justify-center">
-          <h1 className="py-4 text-center text-4xl font-extrabold leading-tight tracking-tight md:text-6xl">
+          <h1 className="py-4 text-center text-3xl font-extrabold leading-tight tracking-tight md:text-6xl">
             {/*<span className="text-amber-500">Motion</span>GPT: Human Motion{" "}*/}
             {/*<br className="hidden sm:inline" />*/}
             {/*as Foreign <span className="text-blue-500">Language</span>*/}
