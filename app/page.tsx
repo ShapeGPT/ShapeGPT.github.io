c2493e12d5edf5cbd8cd31b941fd67ba427793d8
yaic <91652696+fukunyin@users.noreply.github.com>
2023-11-30 16:34:39 +0800

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -189,13 +189,13 @@ export default function IndexPage() {
 
 
       <div className="flex w-full items-end justify-center">
+        <Image src={shapegptLogo} alt="shapegptlogo" width={32}></Image>
           <h1 className="py-4 text-center text-3xl font-extrabold leading-tight tracking-tight md:text-6xl">
             {/*<span className="text-amber-500">Motion</span>GPT: Human Motion{" "}*/}
             {/*<br className="hidden sm:inline" />*/}
             {/*as Foreign <span className="text-blue-500">Language</span>*/}
             <span className="text-sky-500">Shape</span>
             <span className="text-blue-500">GPT:</span> 3D Shape Generation with <br />
-            <Image src={shapegptLogo} alt="shapegptlogo" width={32}></Image>
             A Unified Multi-modal Language Model
           </h1>
           {/*<div className="mt-4 flex items-center justify-center">*/}
